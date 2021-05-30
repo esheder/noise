fcd7549369b0cb24cf8fa978e6cad59e1f058043
Eshed <esheder@gmail.com>
2021-05-30 16:04:21 +0300

    Did a few optimizations and cleanup to avoid unnecessary generator steps.

--- noise/sdesolve.py
+++ noise/sdesolve.py
@@ -5,48 +5,54 @@ dψ = λφdt + σ2dW2
 
 """
 from functools import lru_cache
-from typing import Tuple, Iterable
-from scipy.stats import kurtosis
+from typing import Tuple
+from itertools import takewhile
 
 import numpy as np
+from numba import njit
 
 from .parameters import Parameters
 
 rand_gen = np.random.Generator(np.random.SFC64(48))
 
 
+@njit(nogil=True)
+def _hot_loop(x: np.ndarray, dt: np.ndarray, dx: np.ndarray, α: float) -> None:
+    for i in range(1, len(x)):
+        x[i] = x[i-1] + α*x[i-1]*dt[i-1] + dx[i-1]
+
+
 def signal_make(ts: np.ndarray, par: Parameters, *, randgen: np.random.Generator) -> np.ndarray:
-    x0 = par.equilibrium_flux
     dt = np.diff(ts)
     rands = randgen.normal(0., np.sqrt(dt), (2, len(dt)))
     dnoise = par.σ2 * rands[1, :]
     dx = par.s*dt + par.σ1*rands[0, :] - dnoise
-    x = x0 * np.ones_like(ts, dtype=np.float64)
-    for i in range(1, len(ts)):
-        x[i] = x[i-1] + par.α*x[i-1]*dt[i-1] + dx[i-1]
+    x = par.equilibrium_flux * np.ones_like(ts, dtype=np.float64)
+    _hot_loop(x, dt, dx, par.α)
     y = np.zeros_like(ts, dtype=np.float64)
-    y[1:] = np.cumsum(par.λd*x[:-1]*dt + dnoise)
+    y[1:] = par.λd*x[:-1]*dt + dnoise
     return y
 
 
-def feynman_y_by_signal(t: float, par: Parameters) -> Iterable[Tuple[float, float, float]]:
+def _cut_to_bins(signal: np.ndarray, ts: np.ndarray, skip: int) -> Tuple[float, float, float]:
+    skipsignal = np.diff(signal.cumsum()[::skip])
+    fy = (skipsignal.var(ddof=1) / skipsignal.mean()) - 1.
+    sigma = 1. / np.sqrt(len(skipsignal))
+    return ts[skip], fy, sigma
+
+
+@lru_cache
+def feynman_y_by_signal(par: Parameters, t: float) -> Tuple[np.array, np.array, np.array]:
     dt = min(0.01/abs(par.α), 1e-3)
     n = int(t/dt) + 1
     ts = np.linspace(0., t, n)
     signal = signal_make(ts, par, randgen=rand_gen)
     _times = int(np.log2(n))
-    for stepsize in [2**i for i in range(_times-1)]:
-        skipsignal = signal[::stepsize]
-        skipsignal = np.diff(skipsignal)
-        fy = (skipsignal.var(ddof=1) / skipsignal.mean()) - 1.
-        sigma = 1. / np.sqrt(len(skipsignal))
-        yield ts[stepsize], fy, sigma
+    max_interval = min(1., t/8)
+    resolutions = takewhile(lambda x: dt*x <= max_interval, map(lambda x: 2**x, range(2*_times)))
+    triplets = (_cut_to_bins(signal, ts, skip) for skip in resolutions)
+    dts, fy, sigma = list(zip(*triplets))
+    return np.array(dts, dtype=np.float64), np.array(fy, dtype=np.float64), np.array(sigma, dtype=np.float64)
 
-
-@lru_cache
-def feynman_curve_by_signal(par: Parameters, t: float) -> Tuple[np.array, np.array, np.array]:
-    triplets = list(feynman_y_by_signal(t, par))
-    ts, fy, sigma = list(zip(*triplets))
-    return np.array(ts, dtype=np.float64), np.array(fy, dtype=np.float64), np.array(sigma, dtype=np.float64)
 
 
--- test/test_sde.py
+++ test/test_sde.py
@@ -1,10 +1,13 @@
+from functools import partial
+
 import pytest
 from hypothesis import strategies as st, settings, Phase, given
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 
 from noise import Parameters
 from noise.analytic import fit_to_feynman_y, feynman_y_model
-from noise.sdesolve import feynman_curve_by_signal
+from noise.sdesolve import feynman_y_by_signal
 
 times = st.floats(min_value=60., max_value=600.)
 reactivities = st.floats(min_value=-1000e-5, max_value=-60e-5)
@@ -18,20 +21,23 @@ detections = st.floats(min_value=1e-5, max_value=1e-3)
 @given(t=times, r=reactivities, s=sources, d=detections)
 def test_fitting_signal(t: float, r: float, s: float, d: float):
     par = Parameters.from_dubi(r, 5e-5, s, 2.42, 36., d)
-    ts, curve, sigma = feynman_curve_by_signal(par, t)
+    ts, curve, sigma = feynman_y_by_signal(par, t)
     mask = (ts <= 1e-1)
     tfit, cfit, sfit = ts[mask], curve[mask], sigma[mask]
-    popt, pcov = fit_to_feynman_y(tfit, cfit, None)
-    print('')
-    print(par)
+    popt, pcov = fit_to_feynman_y(tfit, cfit, sfit)
     try:
         assert popt[1] == pytest.approx(-par.α, rel=1e-1)
     except AssertionError:
         fitted = feynman_y_model(ts, *popt)
+        true_alpha_model = partial(feynman_y_model, α=-par.α)
+        true_opt, true_cov = curve_fit(true_alpha_model, ts, curve, 1., sigma=sigma, check_finite=False)
+        true_alpha = true_alpha_model(ts, *true_opt)
         plt.figure()
-        plt.errorbar(ts, curve, yerr=sigma, fmt='.b')
-        plt.plot(ts, fitted, '-r')
+        plt.errorbar(ts, curve, yerr=sigma, fmt='.b', label='Data')
+        plt.plot(ts, fitted, '-r', label="2-parameter fit")
+        plt.plot(ts, true_alpha, '--k', label="Fit with known α")
         plt.grid()
+        plt.legend()
         plt.gca().set_xscale('log')
         plt.savefig('failure.png')
         raise
@@ -40,10 +46,10 @@ def test_fitting_signal(t: float, r: float, s: float, d: float):
 def test_plot_signal():
     t = 60.
     par = Parameters.from_dubi(-500e-5, 5e-5, 1e6, 2.42, 36., 1e-4)
-    ts, curve, _ = feynman_curve_by_signal(par, t)
+    ts, curve, _ = feynman_y_by_signal(par, t)
     popt, pcov = fit_to_feynman_y(ts, curve, None)
     fitted = feynman_y_model(ts, *popt)
-    plt.semilogx(ts, curve, fmt='.b')
+    plt.semilogx(ts, curve, '.b')
     plt.semilogx(ts, fitted, '-r')
     plt.grid()
     plt.show()
